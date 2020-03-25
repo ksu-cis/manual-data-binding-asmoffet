28c5dbe3372f381b096e0f5d2e64ebab4a090a0f
asmoffet <54548893+asmoffet@users.noreply.github.com>
2020-03-25 13:29:57 -0500

    TWO WAY DATA BINDING!!

--- ManualDataBinding/MainWindow.xaml.cs
+++ ManualDataBinding/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ManualDataBinding.Data;
 
 namespace ManualDataBinding
 {
@@ -20,9 +21,43 @@ namespace ManualDataBinding
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// creates the note to edit
+        /// </summary>
+        Note note = new Note();
         public MainWindow()
         {
             InitializeComponent();
+            Editor.Note = note;
+        }
+        /// <summary>
+        /// Event handle to create a new note and apply it to the editor
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void OnNewNote(object sender, RoutedEventArgs e)
+        {
+            note = new Note();
+            Editor.Note = note;
+        }
+        /// <summary>
+        /// clears the note
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void OnClearNote(object sender, RoutedEventArgs e)
+        {
+            note.Body = "";
+        }
+        /// <summary>
+        /// Event handler that mutates a note
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void OnMutateNote(object sender, RoutedEventArgs e)
+        {
+            note.Title = "Master Splinter";
+            note.Body = "ther is no monster more dangerouse than a lack of compassion";
         }
     }
 }
